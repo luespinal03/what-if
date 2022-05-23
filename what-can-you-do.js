a58c68f92ade6642574366ffeb03b06fc121ad9f
luespinal03 <luisespinal294@gmail.com>
2022-05-23 16:50:02 -0400

    what can you do project

--- what-can-you-do.js
+++ what-can-you-do.js
@@ -12,8 +12,14 @@ if (age <= 15) {
 else if (age <= 17) {
     console.log("You can drive but not vote");
 }
+else if (age >= 17) {
+    console.log("You can drive but not vote")
+}
 else if (age <= 24) {
     console.log("You can vote but not rent a car"); 
+}
+else if (age >= 24) {
+    console.log("You can vote but not rent a car");
 }
 else if (age >= 25) {
     console.log("You can do pretty much anything")
