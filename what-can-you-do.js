fc592229262ef70c45c428601f8a29f8177aeacf
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-05-24 11:49:34 -0700

    what can you do project

--- what-can-you-do.js
+++ what-can-you-do.js
@@ -2,22 +2,19 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let age = Number(prompt("Enter your age: ")); // Number here allows for a range
 
-if (age <= 15) {
+if (age < 16) {
     console.log("You can't drive"); 
 }
-else if (age <= 17) {
+else if (age === 16) {
     console.log("You can drive but not vote");
 }
-else if (age >= 17) {
+else if (age === 17) {
     console.log("You can drive but not vote")
 }
 else if (age <= 24) {
     console.log("You can vote but not rent a car"); 
 }
-else if (age >= 24) {
-    console.log("You can vote but not rent a car");
-}
-else if (age >= 25) {
+else { // else if (age >= 25)
     console.log("You can do pretty much anything")
 }
 
