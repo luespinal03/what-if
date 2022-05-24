b68d60d722c7f9635e35b09ed047d53604e42254
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-05-23 21:11:06 -0700

    fizzbuzz project

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,17 @@
 const prompt = require('prompt-sync')({sigint: true});
 
 let num1 = Number(prompt("Enter a number: "));
+let num2 = 3;
+let num3 = 5;
 
-if ( num1 === ) {
-    console.log("Monday");
+if (num1 % num2 === 0 ) {
+    console.log("fizz");
+}
+
+else if (num1 % num3 === 0) {
+    console.log("buzz");
+}
+
+if (num1 % num2 % num2 === 0) {
+    console.log("Fizzbuzz");
 }
